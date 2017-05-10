85fca1dfe9bdb09fdedf31563f6d7270317e7dcb
Enrique Garcia <enrique.garcia@electronicacom.com>
2017-05-09 23:40:46 -0500

    Initialize UKF
    To do:
    	-Update state uppon first measurement for radar and laser

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -51,7 +51,64 @@ UKF::UKF() {
   Complete the initialization. See ukf.h for other member properties.
 
   Hint: one or more values initialized above might be wildly off...
+  s = default
+  m = modiffied
+  d = deffined
+  c = Calculated at later step
+
+  [d] is_initialized_
+  [s] use_laser_
+  [s] use_radar_
+  [ ] x_
+  [d] P_
+  [ ] Xsig_pred_
+  [d] time_us
+  [m] std_a
+  [m] sdt_yawdd_
+  [s] std_laspx_
+  [s] std_laspy_
+  [s] std_radr_
+  [s] std_radphi_
+  [s] std_radrd_
+  [ ] weights
+  [d] n_x_
+  [d] n_aug_
+  [d] lambda_
+  [ ] NIS_radar_
+  [ ] Nis_laser_
+
   */
+  //Typical bicycle accel
+  //https://www.researchgate.net/publication/223922575_Design_speeds_and_acceleration_characteristics_of_bicycle_traffic_for_use_in_planning_design_and_appraisal
+  std_a_ = .355;
+  std_yawdd_ = 5.52;
+  is_initialized_ = false;
+  //elapsed time (us)
+  time_us_= 0;
+  //state size
+  n_x_ = 5;
+  //augmented state size
+  n_aug_ = 7;
+  //Spreading param
+  lambda = 3 - n_x_;
+  //Sensor noise
+  R_laser = MatrixXd(2,2);
+  R_laser <<  std_laspx_*std_laspx_, 0,
+              0, std_laspy_*std_laspy_;
+  R_radar = MatrixXd(3,3);
+  R_radar <<  std_radr_*std_radr_, 0, 0,
+              0, std_radphi_*std_radphi_, 0,
+              0, 0, std_radrd_*std_radrd_;
+
+  //Uncertainity x (0.2m on posx&posy, 0.2m/s vel, 0.1 rad in yaw&yaw_dot)
+  P_ = MatrixXd(n_x_, n_x_);
+  P_ << 0.2, 0, 0, 0, 0,
+        0, 0.2, 0, 0, 0,
+        0, 0, 0.2, 0, 0,
+        0, 0, 0, 0.1, 0,
+        0, 0, 0, 0, 0.1;
+
+
 }
 
 UKF::~UKF() {}
@@ -67,6 +124,21 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if(!is_initialized_){
+    cout << "UKF: " << endl;
+    //Starting state
+    x_ = VectorXd(n_x_);
+    x_.fill(1);
+    if (measurement_pack.sensor_type_ == MeasurementPackage::Radar){
+      float ro = measurement_pack.raw_measurements_(0);
+      float phi = measurement_pack.raw_measurements_(1);
+      float ro_dot = measurement_pack.raw_measurements_(2);
+      
+    }
+
+
+  }
+
 }
 
 /**
